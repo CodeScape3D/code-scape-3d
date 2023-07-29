765e2658b098dea5c37a4d4c03f25321846fc433
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-07-29 13:26:18 -0500

    Inicializacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-center">CodeScape3D + React + Vite + Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
