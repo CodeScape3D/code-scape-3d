c3954d76d12dfbdf4c4931d6407830bbaed4b042
Marco Portacio <marcoportacio4@gmail.com>
2023-08-03 11:02:47 -0500

    Se ha agregado la paleta de colores

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-white">
-      <h1 className="text-center font-black text-3xl animate__animated animate__rotateIn text-purple-900">
-        CodeScape3D + React + Vite + Tailwind
+    <div className="flex justify-center items-center h-screen  bg-primary">
+      <h1 className="text-center font-black text-3xl animate__animated animate__rotateIn text-purple-900 text-white">
+        CodeScape3D + <span className="text-tertiary">React</span> + <span className="text-secondary">Vite</span> + Tailwind
       </h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["index.html", "./src/**/*.jsx"],
   theme: {
     extend: {},
+    colors: {
+      "primary": "#31293B",
+      "secondary": "#715979",
+      "tertiary": "#ECE6E4",
+      "white": "#FFFFFF",
+    }
   },
   plugins: [],
 };
