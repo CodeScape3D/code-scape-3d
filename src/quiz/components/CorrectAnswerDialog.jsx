4c61292fffd1f0387b289a3d2074647875405a1e
Marco Portacio <marcoportacio4@gmail.com>
2023-08-05 15:30:18 -0500

    Se han añadido animaciones a los componentes de Correct y Wrong Dialog

--- src/quiz/components/CorrectAnswerDialog.jsx
+++ src/quiz/components/CorrectAnswerDialog.jsx
@@ -1,6 +1,6 @@
 
 export const CorrectAnswerDialog = () => {
   return (
-    <div className="bg-primary py-3 w-full text-center text-white rounded-lg">Respuesta correcta!</div>
+    <div className="bg-success py-3 w-full text-center text-white rounded-lg animate__animated animate__fadeIn">Respuesta correcta!</div>
   )
 }
--- src/quiz/components/WrongAnswerDialog.jsx
+++ src/quiz/components/WrongAnswerDialog.jsx
@@ -2,6 +2,6 @@
 
 export const WrongAnswerDialog = () => {
     return (
-        <div className="bg-danger py-3 w-full text-center text-white rounded-lg">Respuesta incorrecta :C</div>
+        <div className="bg-danger py-3 w-full text-center text-white rounded-lg animate__animated animate__fadeIn">Respuesta incorrecta :C</div>
     )
 }
