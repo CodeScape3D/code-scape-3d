df76e5261e879f2eaff9f4570f5c2130a6844703
AndresSuarezz <anfesuguito22@gmail.com>
2023-08-05 17:05:20 -0500

    Temas de Cartas y filtracion por curso

--- src/components/Curso.jsx
+++ src/components/Curso.jsx
@@ -1,42 +1,25 @@
-import { svgProgramacion1 } from "../assets/svg/SvgConstans";
-const Curso = () => {
+import PropTypes from "prop-types";
+const Curso = ({ curso, setFiltro }) => {
+  const { imagen, titulo, value } = curso;
+
   return (
     <>
-      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center
-      shadow-xl animate__bounceIn
-      ">
-        <div className="flex justify-center w-10 h-10 md:w-auto">
-          {svgProgramacion1}
-        </div>
+      <div
+        onClick={() => setFiltro(value)}
+        className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center shadow-xl animate__bounceIn"
+      >
+        <div className="flex justify-center w-10 h-10 md:w-auto">{imagen}</div>
         <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
-          Programacion l
+          {titulo}
         </p>
       </div>
-
-      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center
-      shadow-xl animate__bounceIn
-      ">
-        <div className="flex justify-center w-10 h-10 md:w-auto">
-          {svgProgramacion1}
-        </div>
-        <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
-          Programacion ll
-        </p>
-      </div>
-
-      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center
-      shadow-xl animate__bounceIn
-      ">
-        <div className="flex justify-center w-10 h-10 md:w-auto">
-          {svgProgramacion1}
-        </div>
-        <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
-          Programacion lll
-        </p>
-      </div>
-
     </>
   );
+};
+
+Curso.propTypes = {
+  curso: PropTypes.object.isRequired,
+  setFiltro: PropTypes.func,
 };
 
 export default Curso;
