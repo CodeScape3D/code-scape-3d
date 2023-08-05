d9248f342637aff4f3d457abae9859a6a7f966c3
AndresSuarezz <anfesuguito22@gmail.com>
2023-08-04 23:57:02 -0500

    Estilo de Home

---
+++ src/components/Curso.jsx
@@ -0,0 +1,36 @@
+import { svgProgramacion1 } from "../assets/svg/SvgConstans";
+const Curso = () => {
+  return (
+    <>
+      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center">
+        <div className="flex justify-center w-10 h-10 md:w-auto">
+          {svgProgramacion1}
+        </div>
+        <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
+          Programacion l
+        </p>
+      </div>
+
+      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center">
+        <div className="flex justify-center w-10 h-10 md:w-auto">
+          {svgProgramacion1}
+        </div>
+        <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
+          Programacion ll
+        </p>
+      </div>
+
+      <div className="bg-primary cursor-pointer hover:bg-secondary transition-colors rounded-lg w-28 h-28 md:h-44 md:w-44 h-33 flex flex-col justify-center items-center">
+        <div className="flex justify-center w-10 h-10 md:w-auto">
+          {svgProgramacion1}
+        </div>
+        <p className="text-white text-center mt-4 text-xs px-2 md:text-lg mb-2">
+          Programacion lll
+        </p>
+      </div>
+      
+    </>
+  );
+};
+
+export default Curso;
---
+++ src/components/Cursos.jsx
@@ -0,0 +1,13 @@
+import Curso from "./Curso";
+
+const Cursos = () => {
+  return (
+    <>
+      <div className="mt-10 flex flex-row gap-3 md:gap-10 md:mx-auto items-center justify-center">
+        <Curso/>
+      </div>
+    </>
+  );
+};
+
+export default Cursos;
