4627c146e2db55c69b129d9393c22a3ae239cbde
Marco Portacio <marcoportacio4@gmail.com>
2023-08-05 11:25:02 -0500

    Se ha agregado una clase active para los NavTab

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export const NavTab = ({ text, onClick = () => {}, iconSrc = "" }) => {
     return (
-        <button className="nav-tab" onClick={onClick}>
+        <button className="nav-tab nav-tab" onClick={onClick}>
             <span className="text-2xl md:text-base">{text}</span>
             {
                 iconSrc !== "" && <img src={iconSrc} alt="UserLogo" />
