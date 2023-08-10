4fad1bd2f07aae415bc67becc2200c9abda6b568
Marco Portacio <marcoportacio4@gmail.com>
2023-08-10 09:55:33 -0500

    Se ha agregado la acción para calificar un quiz

--- src/quiz/actions/quizActions.js
+++ src/quiz/actions/quizActions.js
@@ -4,4 +4,5 @@ export const quizActions = {
     nextQuestion: "NEXT_QUESTION",
     previousQuestion: "PREVIOUS_QUESTION",
     answerSelected: "ANSWER_SELECTED",
+    checkAnswer: "CHECK_ANSWER",
 }
