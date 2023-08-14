882b4be48b5b54510569e097fd8ced339942b7e8
Marco Portacio <marcoportacio4@gmail.com>
2023-08-14 12:10:29 -0500

    Se ha eliminado la carpeta reducers y actions dado que ya no se van a utilizar

--- src/quiz/actions/quizActions.js
+++
@@ -1,9 +0,0 @@
-
-
-export const quizActions = {
-    nextQuestion: "NEXT_QUESTION",
-    previousQuestion: "PREVIOUS_QUESTION",
-    answerSelected: "ANSWER_SELECTED",
-    checkAnswer: "CHECK_ANSWER",
-    computeResults: "COMPUTE_RESULTS"
-}
