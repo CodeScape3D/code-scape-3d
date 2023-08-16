a05925ebbd0f26515dc01dfa656d0a4a9647d1ed
Marco Portacio <marcoportacio4@gmail.com>
2023-08-16 11:18:47 -0500

    Iniciando mocks de archivos para poner en marcha el ambiente de pruebas

---
+++ src/tests/store/quiz/quizSlice.test.js
@@ -0,0 +1,10 @@
+import { quizSlice } from "../../../store/quiz/quizSlice"
+
+
+describe("Pruebas en el quizSlice", () => {
+
+    test("Debe retornar el estado inicial", () => {
+        quizSlice  
+    })
+
+})
