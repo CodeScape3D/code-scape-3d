319bbbecf08284da4d4dedf34ef5c6a6b7eef829
Marco Portacio <marcoportacio4@gmail.com>
2023-08-19 11:52:32 -0500

    Modificado el componente CardTabla añadiendole un parametro opcional para cambiar su ruta, facilitando su reutilización

--- src/components/CardTabla.jsx
+++ src/components/CardTabla.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const CardTabla = ({ tema }) => {
+const CardTabla = ({ tema, ruta = "" }) => {
   const { titulo, imagen, param } = tema;
 
   return (
-    <Link to={`/animacion/${param}`}>
+    <Link to={ruta === "" ? `/animacion/${param}` : ruta }>
       <div className="hover:scale-110 bg-secondary w-32 mb-2 h-32 p-4 rounded-lg flex justify-center items-center flex-col cursor-pointer animate__bounceIn">
         <div className="">{imagen}</div>
         <p className="text-sm text-white">{titulo}</p>
@@ -16,6 +16,7 @@ const CardTabla = ({ tema }) => {
 
 CardTabla.propTypes = {
   tema: PropTypes.object.isRequired,
+  ruta: PropTypes.string,
 };
 
 export default CardTabla;
--- src/quiz/pages/SortingMethodsPage.jsx
+++ src/quiz/pages/SortingMethodsPage.jsx
@@ -1,9 +1,18 @@
+import CardTabla from "../../components/CardTabla"
+import { topic } from "../../components/HomeCartas"
 
 
 export const SortingMethodsPage = () => {
   return (
     <div className="h-full w-full">
-        <h1>Sorting Methods Page</h1>
+        <h1 className="text-3xl font-bold text-center text-white bg-primary py-4">Métodos de ordenamiento</h1>    
+            <div className="flex">
+                {
+                    topic.map((tema) => (
+                        <CardTabla tema={tema} key={tema.id} ruta={`/quiz/${tema.param}`} />
+                    ))
+                }
+            </div>
     </div>
   )
 }
