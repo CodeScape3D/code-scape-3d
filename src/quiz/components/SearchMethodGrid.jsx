ef8e38237612190ac3518d46c33e3562d693c102
Marco Portacio <marcoportacio4@gmail.com>
2023-08-19 12:04:25 -0500

    Añadido una nueva clase de estilo llamada wrap

---
+++ src/quiz/components/SearchMethodGrid.jsx
@@ -0,0 +1,9 @@
+
+
+export const SearchMethodGrid = ({children}) => {
+  return (
+    <div className="flex justify-evenly items-center flex-wrap">
+        {children}
+    </div>
+  )
+}
--- src/quiz/pages/SortingMethodsPage.jsx
+++ src/quiz/pages/SortingMethodsPage.jsx
@@ -1,18 +1,21 @@
 import CardTabla from "../../components/CardTabla"
 import { topic } from "../../components/HomeCartas"
+import { SearchMethodGrid } from "../components"
 
 
 export const SortingMethodsPage = () => {
-  return (
-    <div className="h-full w-full">
-        <h1 className="text-3xl font-bold text-center text-white bg-primary py-4">Métodos de ordenamiento</h1>    
-            <div className="flex">
-                {
-                    topic.map((tema) => (
-                        <CardTabla tema={tema} key={tema.id} ruta={`/quiz/${tema.param}`} />
-                    ))
-                }
+    return (
+        <div className="h-full w-full">
+            <h1 className="text-3xl font-bold text-center text-white bg-primary py-4">Métodos de ordenamiento</h1>
+            <div className="wrap">
+                <SearchMethodGrid>
+                    {
+                        topic.map((tema) => (
+                            <CardTabla tema={tema} key={tema.id} ruta={`/quiz/${tema.param}`} />
+                        ))
+                    }
+                </SearchMethodGrid>
             </div>
-    </div>
-  )
+        </div>
+    )
 }
