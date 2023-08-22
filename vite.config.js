1c4a0e48c62e1e627087ef55ab156e4284c0e517
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-08-22 01:16:45 -0500

    se implemento PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "CodeScape3D",
+        short_name: "CodeScape",
+        description: "Navega por el codigo como nunca antes con CodeScape3D",
+        icons: [
+          {
+            src: "icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
