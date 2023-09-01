92b08428bbc022a5fee8521a1505d9c88f57b260
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-08-31 23:11:21 -0500

    el codesort se cambio a negrita y fondo

--- src/animations/sorting/components/SortCode.jsx
+++ src/animations/sorting/components/SortCode.jsx
@@ -39,7 +39,7 @@ export const SortCode = () => {
 
   return (
     <div className='w-full md:w-80 mx-auto md:mr-4 md:mb-4'>
-      <div className="bg-secondary text-white py-1 px-2">
+      <div className="bg-gray-900 text-white font-bold py-1 px-2">
         Algoritmo
       </div>
       <div className="bg-primary text-white text-xs p-2">
